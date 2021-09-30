7b4bc5e769433d19e88969c83a15560761d879a8
TrisharTS <67043144+TrisharTS@users.noreply.github.com>
2021-10-01 00:29:49 +0530

    Update Hackfile2.cpp

--- Hackfile2.cpp
+++ Hackfile2.cpp
@@ -2,19 +2,15 @@
 using namespace std;
 
 int fact(int n) {
-    int factorial=1;
-    for(int i=2;i<=n;i++)
-    {
-        factorial*=i;
-    }
-    return factorial;
+    
+    return n*fact(n-1);
 }
 
 int main()
 {
     int row;
     cout<<"Enter the no of rows";
-    cin>>row;
+    cin>>r;
     for(int i=0;i<row;i++) {
         for(int j=0;j<=i;j++) {
             cout<<fact(i)/(fact(i-j)*fact(j))<<" ";
