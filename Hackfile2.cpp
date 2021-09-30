cade5e12ef1f930d273ee66e4ef55d719cff0edc
Thenewslogan <91694795+Thenewslogan@users.noreply.github.com>
2021-10-01 00:05:40 +0530

    Create Hackfile2.cpp

---
+++ Hackfile2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n) {
+    int factorial=1;
+    for(int i=2;i<=n;i++)
+    {
+        factorial*=i;
+    }
+    return factorial;
+}
+
+int main()
+{
+    int row;
+    cout<<"Enter the no of rows";
+    cin>>row;
+    for(int i=0;i<row;i++) {
+        for(int j=0;j<=i;j++) {
+            cout<<fact(i)/(fact(i-j)*fact(j))<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
