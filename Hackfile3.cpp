613533da7b15746bdc45fc7a0120ee54a8709aed
TrisharTS <67043144+TrisharTS@users.noreply.github.com>
2021-10-01 00:39:19 +0530

    Infix to postfix fixed for space gap

--- Hackfile3.cpp
+++ Hackfile3.cpp
@@ -1,25 +1,85 @@
-//Fibonacci Series upto n
-
-#include <iostream>
-
-using namespace std;
-
-void fib(int n) {
-    int t1=0,t2=1,next;
-    for(int i=1;i<=n;i++) {
-        cout<<t1<<" ";
-        next = t1+t2;
-        t1=t2;
-        t2=next;
-    }
-}
-
-int main()
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define Max 100
+char infix[100],postfix[100],stack[Max],pr,c;
+int top=-1,item,i,k;
+void push(char c)
 {
-    int n;
-    cout<<"Enter n";
-    cin>>n;
-    cout<<"Fibonacci series upto "<<n<<endl;
-    fib(n);
-    return 0;
+stack[++top]=c;
+}
+char pop(c)
+{
+ return stack[top--];
+}
+int precedence (char elem)
+{  
+switch(elem)
+{
+    case '#' :return 0;
+          break; 
+    case '(' :return 1;
+          break; 
+    case '+' :return 2;
+          break; 
+    case '-' :return 2;
+          break; 
+    case '*' :return 3;
+          break; 
+    case '/' :return 3;
+          break; 
+    case '%' :return 3;
+          break;
+     case '^' :return 4;
+          break;  
+    default: return -1;
+         break;
+}
+}
+void main()
+{
+char ch;
+printf("Enter the infix expression");
+scanf("%s",infix);
+push('#');
+for(i=0;infix[i]!='\0';i++)
+ {
+   if(infix[i]=='(')
+   {
+    push(infix[i]);
+   }
+    else if(infix[i]==')')
+    {
+     while(stack[top]!='(')
+     {
+       postfix[k++]=pop(stack[top]);
+     }
+       pop(infix[i]);
+     }
+     else if(isalnum(infix[i]))
+    {
+     postfix[k++]=infix[i];
+    }
+    else
+    {
+     pr=precedence(infix[i]);
+     if(precedence(stack[top]>=pr))
+     {
+       push(infix[i]);
+     }
+      else
+     {
+      postfix[k++]=pop(stack[top]);
+      push(infix[i]);
+     }
+    }
+ }
+
+while(stack[top]!='#')
+{
+ postfix[k++]=stack[top];
+ top--;
+ }
+
+puts(postfix);
 }
